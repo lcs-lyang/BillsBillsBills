ec38f8cd181af9d912c477aa0b34e9cd7d123be0
Lillian Yang <lyang@lcs.on.ca>
2022-05-06 15:08:41 -0400

    Initial Commit

---
+++ BillsBillsBills/BillsBillsBillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillsBillsBillsApp.swift
+//  BillsBillsBills
+//
+//  Created by Lillian Yang on 2022-05-06.
+//
+
+import SwiftUI
+
+@main
+struct BillsBillsBillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillsBillsBills/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BillsBillsBills
+//
+//  Created by Lillian Yang on 2022-05-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
