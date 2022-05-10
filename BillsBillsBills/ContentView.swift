1a90e2e888fc2b7fc7104d51fd55bc3438dd9fa9
Lillian Yang <lyang@lcs.on.ca>
2022-05-10 09:46:28 -0400

    Started the Static Layout for Cost Estimation View

--- BillsBillsBills/ContentView.swift
+++ BillsBillsBills/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack{
+            
+            VStack (alignment: .leading){
+                
+                DatePicker("Date:",
+                           selection: .constant(Date()),
+                           displayedComponents: .date)
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Food:")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Drinks:")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Dessert:")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Spacer()
+            }
+            Spacer()
+        }
+        .navigationTitle("Cost Estimation")
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
