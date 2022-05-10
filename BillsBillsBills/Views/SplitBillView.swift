71f3cc8811fdb1818d124916c016cf8dfd870704
Lillian Yang <lyang@lcs.on.ca>
2022-05-10 09:54:03 -0400

    Created the SplitBill view.

---
+++ BillsBillsBills/Views/SplitBillView.swift
@@ -0,0 +1,20 @@
+//
+//  SplitBillView.swift
+//  BillsBillsBills
+//
+//  Created by Lillian Yang on 2022-05-10.
+//
+
+import SwiftUI
+
+struct SplitBillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplitBillView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplitBillView()
+    }
+}
