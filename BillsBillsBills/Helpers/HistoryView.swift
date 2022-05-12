9d18d89ac2d97afad3f20614dca9c1fa5a1cf414
Lillian Yang <lyang@lcs.on.ca>
2022-05-12 19:27:57 -0400

    Created a history view to store the calculations

---
+++ BillsBillsBills/Helpers/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  BillsBillsBills
+//
+//  Created by Lillian Yang on 2022-05-12.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
